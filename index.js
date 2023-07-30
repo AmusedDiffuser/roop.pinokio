17138854c02f85068349e0dcb2220deb64676ebd
AmusedDiffuser <81667911+AmusedDiffuser@users.noreply.github.com>
2023-07-30 19:30:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,36 +6,22 @@ class Roop {
 
     // Clone repo
     await kernel.shell.run('git clone https://github.com/s0md3v/roop', {cwd: path.resolve(__dirname)})
+    
+    // Create venv
+    await kernel.shell.run('python -m venv env', {cwd: path.resolve(__dirname, 'roop')})
 
-    // Create and activate venv
+    // Activate venv
     if (process.platform === 'win32') {
-      await kernel.shell.run(`python -m venv env`, {cwd: path.resolve(__dirname, 'roop')})  
       await kernel.shell.enter('env\\Scripts\\activate.bat', {cwd: path.resolve(__dirname, 'roop')})
-    } else {
-      await kernel.shell.run(`python3 -m venv env`, {cwd: path.resolve(__dirname, 'roop')})
+    } else { 
       await kernel.shell.enter('source env/bin/activate', {cwd: path.resolve(__dirname, 'roop')})
     }
 
     // Install requirements
     await kernel.pip.install('-r requirements.txt', {cwd: path.resolve(__dirname, 'roop')})
 
-    // Notify success
-    await kernel.input.form({
-      title: 'Roop Installed',
-      description: 'Installation complete! You can now run roop.'
-    })
-
   }
 
-  async run(req, ondata, kernel) {
-    let args = req.params || {}
-    if (process.platform === 'win32') {
-      await kernel.python(path.resolve(__dirname, 'roop/run.py'), args, {cwd: path.resolve(__dirname)}) 
-    } else {
-      await kernel.python(path.resolve(__dirname, 'roop/run.py'), args, {cwd: path.resolve(__dirname)})
-    }
-  }
+  //...other methods 
 
 }
-
-module.exports = Roop
