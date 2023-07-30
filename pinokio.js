f8cfa4457e594ed7aeccdf3f0ce187044489d1c7
AmusedDiffuser <81667911+AmusedDiffuser@users.noreply.github.com>
2023-07-30 19:21:50 -0400

    Update pinokio.js

--- pinokio.js
+++ pinokio.js
@@ -1,40 +1,10 @@
-const path = require('path')
-const { existsSync } = require('fs')
-const { writeFileSync } = require('fs')
-
 module.exports = {
 
-  //...other functions  
-
-  menu: async (kernel) => {
-
-    let menuObject
-
-    try {
-
-      let installed = module.exports.isInstalled()
-
-      if (installed) {
-        menuObject = {
-          html: "<i class='fa-solid fa-check'></i> Roop Installed",
-          href: "run.json"
-        }
-      } else {
-        menuObject = {
-          html: "<i class='fa-solid fa-arrow-down'></i> Install Roop",
-          href: "install.json"
-        }
-      }
-
-    } catch (err) {
-      console.error('Error generating menu', err)
+  menu: [
+    {
+      html: "<i class='fa-solid fa-rocket'></i>&nbsp;Start",
+      href: "launch.json",
     }
-    
-    // Log menu object
-    writeFileSync('pinokio-menu.log', JSON.stringify(menuObject))
-
-    return menuObject
-
-  }
+  ]
 
 }
