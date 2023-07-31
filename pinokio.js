e24bf95811e3f8aa978a50bd9004830cc6bf0708
AmusedDiffuser <81667911+AmusedDiffuser@users.noreply.github.com>
2023-07-30 20:16:15 -0400

    Update pinokio.js

--- pinokio.js
+++ pinokio.js
@@ -2,14 +2,11 @@ module.exports = {
   title: "roop",
   description: "One click face-swap GUI",
   icon: "icon.png",
-  update: async (kernel) => {
-    return "update.json"
-  },
   menu: [{
     html: '<i class="fa-solid fa-microchip"></i> Install',
     href: "install.json"
   }, {
     html: '<i class="fa-solid fa-rocket"></i> Launch',
-    href: "start.json"
+    href: "run.json"
   }]
 }
